c948aa6c442df1bba52314d076034dc39374ff7c
Ye Chuah <yechuah@gmail.com>
2012-04-03 23:35:57 +1000

    cleaned up

--- stgeorge.pl
+++ stgeorge.pl
@@ -1,21 +1,36 @@
 #!/usr/bin/perl
 use WWW::Mechanize;
+
+$userid = '';
+$securitynumber = '';
+$password = '';
+$accountpos = 3; # The position of the account on the my accounts page
+
+# Login url
 $url = 'https://ibanking.stgeorge.com.au/InternetBanking/welcome.jsp';
+
 my $m = WWW::Mechanize->new();
 $m->get($url);
 $m->form_name('logonForm');
-$m->field('userId', '');
-$m->field('securityNumber', '');
-$m->field('password', '');
+$m->field('userId', $userid);
+$m->field('securityNumber', $securitynumber);
+$m->field('password', $password);
 $response = $m->submit();
 #print $response->content();
 
-$m->follow_link( url_regex => qr/viewAccountDetails/i, n => 3 );
+$m->form_name('splashScreenForm');
+$m->submit();
+$m->follow_link( url_regex => qr/viewAccountDetails/i, n => $accountpos );
+#print $m->content();
+
+$m->submit_form(
+    form_name => 'accountDetailForm',
+    fields => {selectedOption => 3}
+);
+
+$m->submit_form(
+    form_name => 'accountDetailForm',
+    fields => {dispatchCommand => 'downloadButton'}
+);
+
 print $m->content();
-
-
-#$m->submit_form(
-#    form_name => 'accountDetailForm',
-#    fields => {selectedOption => 3}
-#);
-
